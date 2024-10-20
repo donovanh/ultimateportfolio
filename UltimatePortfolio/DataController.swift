8df8285a54032d5be91b16f6ac8f2dd0c79ded9f
Donovan Hutchinson <donovanh@gmail.com>
2024-10-20 21:11:33 +0100

    Adding German

--- UltimatePortfolio/DataController.swift
+++ UltimatePortfolio/DataController.swift
@@ -203,13 +203,13 @@ class DataController: ObservableObject {
     func newTag() {
         let tag = Tag(context: container.viewContext)
         tag.id = UUID()
-        tag.name = NSLocalizedString("New tag", comment: "Create a new tag")
+        tag.name = String(localized: "New tag")
         save()
     }
     
     func newIssue() {
         let issue = Issue(context: container.viewContext)
-        issue.title = NSLocalizedString("New issue", comment: "Create a new issue")
+        issue.title = String(localized: "New issue")
         issue.creationDate = .now
         issue.priority = 1
         
